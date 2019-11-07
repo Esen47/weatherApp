28bfac7c5c0b3c72777c0a038752af0a507807c7
Sasa Nikolic <sasa.nikolic225@gmail.com>
2019-11-07 17:40:44 -0500

    updating the UI with the info from the weather API

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,35 @@
 const addForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+//function to update the UI with weather information
+const updateUI = (data) => {
+    console.log(data);
+    //destructure properties
+    const{ cityDetails, weatherDetails } = data;
+
+    details.innerHTML = `
+        <h5 class="my-3">${cityDetails.EnglishName}</h5>
+        <div class="my-3">${weatherDetails.WeatherText}</div>
+        <div class="display-4 my-4">
+        <span>${weatherDetails.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+        </div>
+    `;
+    //update the night/day & icon images
+    const iconSrc = `../img/icons/${weatherDetails.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    //setting condition using ternary operator
+    let timeSrc = weatherDetails.IsDayTime ? 'img/day.svg' : 'img/night.svg'; 
+    time.setAttribute('src', timeSrc);
+
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+}
 
 const getWeatherInfo = async (city) => {
     const cityDetails = await getCity(city);
@@ -16,6 +47,6 @@ addForm.addEventListener('submit', e => {
     addForm.reset();
     //calling the getWeather function using user input
     getWeatherInfo(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err));
 });
